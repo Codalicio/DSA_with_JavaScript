0c898b541dde81b9eab67fca2392c9434574c665
Amit Raj <rajamitnine@gmail.com>
2025-07-23 23:22:25 +0530

    Improved the logic I thought and applied

--- Week_1/DSA_Introduction_and_Loop_mastery/printingPatterns.js
+++ Week_1/DSA_Introduction_and_Loop_mastery/printingPatterns.js
@@ -4,7 +4,7 @@
 // * * * *
 // * * * * *
 
-// * Process : Take a loop as that changes rows after every complete execution of the loop, then inside that iteration print number of stars you want.
+// * Process : Take a outer loop that changes rows after every execution of the loop, take an inner loop, then the inner loop will print the number of stars in a row based on the inner loop.
 
 for (let i = 1; i <= 5; i++) {
   for (let j = 1; j <= i; j++) {
